c0dc5fb6bf2f9d9ff50278bfd34dda6d277f862d
basaktuysuz1 <85128377+basaktuysuz@users.noreply.github.com>
2023-08-25 16:02:43 +0300

    Maps Sdk

---
+++ Mapbox Maps Examples/MapBox Display Building Extrusions/app/src/main/java/com/example/mapboxanimatedweatherdata/FillExtrusionActivity.kt
@@ -0,0 +1,84 @@
+package com.example.mapboxanimatedweatherdata
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mapboxanimatedweatherdata.databinding.ActivityFillExtrusionBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.eq
+import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.get
+import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.literal
+import com.mapbox.maps.extension.style.layers.addLayer
+import com.mapbox.maps.extension.style.layers.generated.FillExtrusionLayer
+import com.mapbox.maps.extension.style.light.generated.getLight
+
+/**
+ * Extrude the building layer in the Mapbox Light style using FillExtrusionLayer
+ * and set up the light position.
+ */
+class FillExtrusionActivity : AppCompatActivity() {
+
+    private var isRedColor: Boolean = false
+    private var isInitPosition: Boolean = false
+    private lateinit var binding: ActivityFillExtrusionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFillExtrusionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mapboxMap = binding.mapView.getMapboxMap()
+
+        mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(-74.0066, 40.7135))
+                .pitch(45.0)
+                .zoom(15.5)
+                .bearing(-17.6)
+                .build()
+        )
+
+        mapboxMap.loadStyleUri(
+            Style.LIGHT
+        ) { style ->
+            setupBuildings(style)
+            setupLight(style)
+        }
+    }
+
+    private fun setupBuildings(style: Style) {
+        val fillExtrusionLayer = FillExtrusionLayer("3d-buildings", "composite")
+        fillExtrusionLayer.sourceLayer("building")
+        fillExtrusionLayer.filter(eq(get("extrude"), literal("true")))
+        fillExtrusionLayer.minZoom(15.0)
+        fillExtrusionLayer.fillExtrusionColor(Color.parseColor("#aaaaaa"))
+        fillExtrusionLayer.fillExtrusionHeight(get("height"))
+        fillExtrusionLayer.fillExtrusionBase(get("min_height"))
+        fillExtrusionLayer.fillExtrusionOpacity(0.6)
+        fillExtrusionLayer.fillExtrusionAmbientOcclusionIntensity(0.3)
+        fillExtrusionLayer.fillExtrusionAmbientOcclusionRadius(3.0)
+        style.addLayer(fillExtrusionLayer)
+    }
+
+    private fun setupLight(style: Style) {
+        val light = style.getLight()
+        binding.fabLightPosition.setOnClickListener {
+            isInitPosition = !isInitPosition
+            if (isInitPosition) {
+                light.position(1.5, 90.0, 80.0)
+            } else {
+                light.position(1.15, 210.0, 30.0)
+            }
+        }
+
+        binding.fabLightColor.setOnClickListener {
+            isRedColor = !isRedColor
+            if (isRedColor) {
+                light.color(Color.RED)
+            } else {
+                light.color(Color.BLUE)
+            }
+        }
+    }
+}
---
+++ Mapbox Maps Examples/MapBox Tint a fill layer/app/src/main/java/com/example/viewAnnotation/StyleCirclesCategoricallyActivity.kt
@@ -0,0 +1,87 @@
+package com.example.viewAnnotation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mapboxanimatedweatherdata.R
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.extension.style.expressions.dsl.generated.match
+import com.mapbox.maps.extension.style.layers.generated.circleLayer
+import com.mapbox.maps.extension.style.sources.generated.vectorSource
+import com.mapbox.maps.extension.style.style
+
+
+/**
+ * Add point data to a style from a vector tileset and use the match and
+ * get expressions to assign the color of each point in a CircleLayer
+ * based on a data property.
+ */
+class StyleCirclesCategoricallyActivity : AppCompatActivity() {
+
+  private lateinit var mapboxMap: MapboxMap
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val mapView = MapView(this)
+    setContentView(mapView)
+    mapboxMap = mapView.getMapboxMap()
+    mapboxMap.loadStyle(
+      style(Style.LIGHT) {
+
+        +vectorSource("ethnicity-source") {
+          url("http://api.mapbox.com/v4/examples.8fgz4egr.json?access_token=" + getString(R.string.mapbox_access_token))
+        }
+
+        +circleLayer("population", "ethnicity-source") {
+          sourceLayer("sf2010")
+          circleRadius(
+            interpolate {
+              exponential {
+                literal(1.75)
+              }
+              zoom()
+              stop {
+                literal(12)
+                literal(2)
+              }
+              stop {
+                literal(22)
+                literal(180)
+              }
+            }
+          )
+
+          circleColor(
+            match {
+              get {
+                literal("ethnicity")
+              }
+              literal("white")
+              rgb(251.0, 176.0, 59.0)
+              literal("Black")
+              rgb(34.0, 59.0, 83.0)
+              literal("Hispanic")
+              rgb(229.0, 94.0, 94.0)
+              literal("Asian")
+              rgb(59.0, 178.0, 208.0)
+              literal("Other")
+              rgb(204.0, 204.0, 204.0)
+              rgb(0.0, 0.0, 0.0)
+            }
+          )
+        }
+      }
+    )
+
+    mapboxMap.setCamera(
+      CameraOptions.Builder()
+        .center(Point.fromLngLat(-122.447303, 37.753574))
+        .zoom(12.0)
+        .build()
+    )
+  }
+}
